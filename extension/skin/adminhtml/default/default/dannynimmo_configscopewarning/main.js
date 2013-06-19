bffdb848e50199a2398b8b086b0c787edbb3b5ff
dannynimmo <danny@dannynimmo.co.nz>
2013-06-20 00:32:12 +1200

    Tidy up of JS file.

--- extension/skin/adminhtml/default/default/dannynimmo_configscopewarning/main.js
+++ extension/skin/adminhtml/default/default/dannynimmo_configscopewarning/main.js
@@ -1,27 +1,19 @@
-(function(window, document, undefined) {
+(function(document) {
     'use strict';
 
-    var tooltip = {
-
-        init: function() {
-            this.listen();
-        },
-
-        listen: function() {
-            [].forEach.call(document.querySelectorAll('.scope-warning'), function(el) {
-                el.addEventListener('mouseover', function() {
-                    this.getElementsByTagName('div')[0].classList.add('active');
-                });
-                el.addEventListener('mouseout', function() {
-                    this.getElementsByTagName('div')[0].classList.remove('active');
-                });
+    function tooltips() {
+        [].forEach.call(document.querySelectorAll('.scope-warning'), function(el) {
+            el.addEventListener('mouseover', function() {
+                this.getElementsByTagName('div')[0].classList.add('active');
             });
-        }
-
-    };
+            el.addEventListener('mouseout', function() {
+                this.getElementsByTagName('div')[0].classList.remove('active');
+            });
+        });
+    }
 
     document.addEventListener('DOMContentLoaded', function() {
-        tooltip.init();
+        tooltips();
     });
 
-})(window, document);
+})(document);
